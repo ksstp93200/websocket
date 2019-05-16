90f4a7f0a0d695f9693b6a0b755ab896ae802684
ksstp93200 <42488321+ksstp93200@users.noreply.github.com>
2019-05-16 21:21:56 +0800

    Update server.py

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ music_queue = list()
 def new_client(client, server):
     print("Client has joined.")
     send_msg = dict
-    send_msg['type'] = "playlist"
+    send_msg['cmd'] = "playlist"
     send_msg['data'] = music_queue
     server.send_message(json.dumps(send_msg))
 
@@ -20,15 +20,15 @@ def client_left(client, server):
 def message_back(client, server, message):
     send_msg = dict
     rcv = json.loads(message)
-    if(rcv.type == "stop"):
-        send_msg['type'] = "stop"
+    if(rcv.cmd == "stop"):
+        send_msg['cmd'] = "stop"
         server.send_message_to_all(json.dumps(send_msg))
-    if(rcv.type == "play"):
-        send_msg['type'] = "play"
+    if(rcv.cmd == "play"):
+        send_msg['cmd'] = "play"
         server.send_message_to_all(json.dumps(send_msg))
-    if(rcv.type == 'url'):
+    if(rcv.cmd == 'url'):
         music_queue.append(message)
-        send_msg['type'] = "add"
+        send_msg['cmd'] = "add"
         send_msg['data'] = rcv.data
         server.send_message_to_all(json.dumps(send_msg))
 
