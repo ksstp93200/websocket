99398058d7209eadcf70076cc54b202382f4e469
ksstp93200 <42488321+ksstp93200@users.noreply.github.com>
2019-05-17 19:58:05 +0800

    Update server.py

--- server.py
+++ server.py
@@ -10,34 +10,55 @@ mutex = threading.Lock()
 status = False
 
 playtime = None
-
 remaintime = None
 
 port = int(os.environ.get('PORT', 5000))
 
 music_queue = list()
-
-def gettime():
-    localtime = time.localtime(time.time())
-    return localtime
+time_queue = list()
 
 def updatequeue(ptime, ctime):
-    
+    pastsec = int(ctime - ptime)
+    if pastsec >= remaintime:
+        pastsec -= remaintime
+        try:
+            music_queue.pop(0)
+            time_queue.pop(0)
+        except:
+            return -1
+        for i in range(len(music_queue)):
+            if pastsec >= time_queue[0]:
+                pastsec -= time_queue[0]
+                try:
+                    music_queue.pop(0)
+                    time_queue.pop(0)
+                except:
+                    return -1
+            else:
+                return time_queue[0] - pastsec
+        return -1
+    else:
+        return remaintime - pastsec
 
 def new_client(client, server):
     print("Client has joined.")
     send_msg = dict()
     mutex.acquire()
     if(status):
-        currenttime = gettime()
+        currenttime = time.time()
         remaintime = updatequeue(playtime, currenttime)
-        playtime = currenttime
-        send_msg['data'] = copy.copy(music_queue)
-        send_msg['time'] = copy.copy(remaintime)
-        send_msg['status'] = True
+        if remaintime == -1:
+            send_msg['data'] = []
+            send_msg['status'] = False
+            status = False
+        else:
+            playtime = currenttime
+            send_msg['data'] = copy.copy(music_queue)
+            send_msg['time'] = copy.copy(time_queue[0] - remaintime)
+            send_msg['status'] = True
     else:
         send_msg['data'] = copy.copy(music_queue)
-        send_msg['time'] = copy.copy(remaintime)
+        send_msg['time'] = copy.copy(time_queue[0] - remaintime)
         send_msg['status'] = False
     mutex.release()
     send_msg['type'] = "playlist"
@@ -53,20 +74,23 @@ def message_back(client, server, message):
     if(rcv["type"] == "stop" and status):
         mutex.acquire()
         status = False
-        currenttime = gettime()
+        currenttime = time.time()
         remaintime = updatequeue(playtime, currenttime)
         mutex.release()
         send_msg['type'] = "stop"
         server.send_message_to_all(json.dumps(send_msg))
-    if(rcv["type"] == "play" and not status):
+    if(rcv["type"] == "play" and not status and len(music_queue) > 0):
         status = True
         mutex.acquire()
-        playtime = gettime()
+        playtime = time.time()
         mutex.release()
         send_msg['type'] = "play"
         server.send_message_to_all(json.dumps(send_msg))
     if(rcv["type"] == 'url'):
+        mutex.acquire()
         music_queue.append(rcv["data"])
+        mutex.release()
+        time_queue.append(rcv["time"])
         send_msg['type'] = "add"
         send_msg['data'] = rcv["data"]
         server.send_message_to_all(json.dumps(send_msg))
